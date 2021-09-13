71040f4e69cec11f5725d0c5e2705f6a3c52f6f7
CyborgOctopus <pdasedano@gmail.com>
2021-09-12 23:49:33 -0400

    Improved IoU function and cleaned up the test function. Deleted the 'archaic' folder.

--- tornado_damage_segmentation/convnet.py
+++ tornado_damage_segmentation/convnet.py
@@ -32,48 +32,53 @@ test_loader = DataLoader(test_data)
 # and here:
 # https://github.com/qubvel/segmentation_models.pytorch/blob/master/segmentation_models_pytorch/losses/_functional.py
 # (look for 'soft_jaccard_score')
-def iou(prediction, target, soft=False):
+#
+# Also takes inspiration from https://torchmetrics.readthedocs.io/en/stable/references/modules.html?highlight=IoU#iou
+def iou(prediction, target, soft=False, reduce=True, zero_over_zero_score=1):
     # N, C, *dims --> C, N, *dims --> C, N x product(*dims)
     pred_positives = prediction.transpose(0, 1).view(prediction.shape[1], -1)
+    # N, *dims --> N x product(*dims) --> N x product(*dims), C --> C, N x product(*dims)
+    true_positives = F.one_hot(target.view(-1), num_classes=prediction.shape[1]).transpose(0, 1)
+
     if soft:
         pred_positives = pred_positives.softmax(dim=0)
     else:
         # C, N x product(*dims) --> N x product(*dims) --> N x product(*dims), C --> C, N x product(*dims)
         pred_positives = F.one_hot(pred_positives.argmax(axis=0)).transpose(0, 1)
-    # N, *dims --> N x product(*dims) --> N x product(*dims), C --> C, N x product(*dims)
-    true_positives = F.one_hot(target.view(-1), num_classes=prediction.shape[1]).transpose(0, 1)
+
     intersection = torch.sum(pred_positives * true_positives, dim=1)
     union = torch.sum(pred_positives + true_positives, dim=1) - intersection
-    return (intersection / union).mean().item()
+    result = intersection / union
+    result[union == 0] = zero_over_zero_score
+
+    if reduce:
+        return result.mean().item()
+    return result
 
 
 # Tests the neural network, returning average accuracies
-# TODO update accuracy measure.
 def test(verbose=False):
-    pixel_accuracies = []
-    iou_accuracies = []
-    my_iou_accuracies = []
-    my_soft_iou_accuracies = []
+    accuracies = []
     model.eval()
     with torch.no_grad():
         for raster, mask in test_loader:
-            print("Start : %s" % time.ctime())
-            print("raster shape: " + str(raster.shape))
-            print("mask shape: " + str(mask.shape))
+            start_time = time.ctime()
             output = model(raster)
-            print("output shape: " + str(output.shape))
-            pixel_accuracies.append(np.average(output.argmax(axis=1) == mask))
-            iou_accuracies.append(1 - smp.losses.JaccardLoss(mode='multiclass').forward(output, mask).item())
-            my_iou_accuracies.append(iou(output, mask))
-            my_soft_iou_accuracies.append(iou(output, mask, soft=True))
+            end_time = time.ctime()
+            accuracies.append({
+                'Pixel accuracy': np.average(output.argmax(axis=1) == mask),
+                'IoU': iou(output, mask),
+                'IoU by class': iou(output, mask, reduce=False),
+                'Soft IoU': iou(output, mask, soft=True),
+                'Soft IoU by class': iou(output, mask, soft=True, reduce=False)
+            })
             if verbose:
-                print('Pixel Accuracy: ', pixel_accuracies[-1])
-                print('IoU: ', iou_accuracies[-1])
-                print('My IoU: ', my_iou_accuracies[-1])
-                print('My Soft IoU: ', my_soft_iou_accuracies[-1])
-            print("End : %s" % time.ctime() + "\n")
+                print("Start : %s" % start_time)
+                print("End : %s" % end_time)
+                for i, (key, value) in enumerate(accuracies[-1].items()):
+                    print(key + ': ', value)
     model.train()
-    return {'pixel accuracy': np.mean(pixel_accuracies), 'iou': np.mean(iou_accuracies), 'my_iou': np.mean(my_iou_accuracies)}
+    return [np.mean([accuracies[i][key] for i in range(len(accuracies))]) for key in accuracies[-1].keys()]
 
 
 # Train the neural network
